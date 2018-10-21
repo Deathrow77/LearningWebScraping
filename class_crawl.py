89598e9b4e310e3f6667521d7b33ed4e942a92ec
Deathrow77 <parul.chandel14@gmail.com>
2018-10-21 21:55:11 +0530

    Mutiple Site Crawler added

---
+++ class_crawl.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+class Content:
+    def __init__(self, url, title, body):
+        self.url = url
+        self.body = body
+        self.title = title
+def getPage(url):
+    req = requests.get(url)
+    return BeautifulSoup(req.text, 'html.parser')
+
+def scrapeNYtimes(url):
+    bs = getPage(url)
+    title = bs.find('h1').text
+    lines = bs.find_all('p', {'class':'body-content'})
+    body = '\n'.join([line.text for line in lines])
+    return Content(url, title,body)
+
+def scrapeBrookings(url):
+    bs = getPage(url)
+    title = bs.find('h1').text
+    body = bs.find('div', {'class':'post-body'})
+    return Content(url, title, body)
+            
+            
+url = 'https://www.brookings.edu/blog/future-development/2018/01/26/delivering-inclusive-urban-access-3-uncomfortable-truths/'
+
+content = scrapeBrookings(url)
+print('Title : {}'.format(content.title))
+print('URL : {}'.format(content.url))
+print(content.body)
+
+url = 'https://www.nytimes.com/2018/01/25/opinion/sunday/'
+content = scrapeNYtimes(url)
+print('Title : {}'.format(content.title))
+print('URL : {}'.format(content.url))
+print(content.body)
--- site_crawler2.py
+++ site_crawler2.py
@@ -7,13 +7,17 @@ def getExternalLinks(url):
     global pages
     html = urlopen('http://en.wikipedia.org{}'.format(url))
     bs = BeautifulSoup(html, 'html.parser')
-    for link in bs.find_all('a'):
+    for link in bs.find_all('a', href=re.compile('^(/wiki/)')):
         if link.attrs['href'] == re.compile('^(/wiki/)((?!:).)*$'):
             if 'href' in link.attrs:
                 if link.attrs['href'] not in pages:
                     newPage = link.attrs['href']
-                    print(newPage)
+                    print('New Internal Link - ' + newPage)
                     pages.add(newPage)
-                    getLinks(newPage)
-getLinks('')
+                    getExternalLinks(newPage)
+        else:
+            newPage = link.attrs['href']
+            print('New External Link - ' + newPage)
+            getExternalLinks(newPage)
+getExternalLinks('')
                         
